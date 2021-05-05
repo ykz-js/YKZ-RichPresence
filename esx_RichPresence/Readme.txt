7ca3d585b6fd0da060bc7639cf7ab144d3c13fbe
ykz-js <80243710+ykz-js@users.noreply.github.com>
2021-05-05 10:46:00 -0300

    Add files via upload

---
+++ esx_RichPresence/Readme.txt
@@ -0,0 +1,4 @@
+Set BOT ID in discord.lua  SetDiscordAppId()
+Set Logo BIG in SetDiscordRichPresenceAsset("big")
+Set Logo SMALL in SetDiscordRichPresenceAssetSmall("small")
+Page for register bot -- https://discord.com/developers/applications/
---
+++ esx_RichPresence/discord.lua
@@ -0,0 +1,25 @@
+Citizen.CreateThread(function()
+  while true do
+      
+      Citizen.Wait(5*1000) -- Checkea cada 5 segundos
+      
+      SetDiscordAppId(822211057749590086) -- ID Del BOT
+
+      local player = PlayerId()
+        local name = GetPlayerName(player)
+		local id = GetPlayerServerId(player)
+		SetRichPresence(name ..  " Malianteo RP " .. #GetActivePlayers() .. "/48 | ID: " .. id)
+     
+
+     SetDiscordRichPresenceAsset("ql3wjaldanfkxczd6gtsgac5s4") -- nombre logo grande
+    SetDiscordRichPresenceAssetText("https://discord.gg/3nXNx2zBUq") -- Nombre en el logo grande
+
+      SetDiscordRichPresenceAssetSmall("descarga--1-") -- nombre logo pequeño
+      SetDiscordRichPresenceAssetSmallText("YKZ$Q") -- logo pequeño
+      
+        SetDiscordRichPresenceAction(0, "Discord", "https://discord.gg/3nXNx2zBUq")
+        SetDiscordRichPresenceAction(1, "Incoming...", "")
+
+		Citizen.Wait(60000)
+  end
+end)
